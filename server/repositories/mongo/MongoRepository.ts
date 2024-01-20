d249adc464160b724fbdcd6507fcad1e7e4e36d4
vivia1990 <donvivia@gmail.com>
2024-01-20 23:19:51 +0100

    feat(repo): logica selezione campi

--- server/repositories/mongo/MongoRepository.ts
+++ server/repositories/mongo/MongoRepository.ts
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // todo pensare a fix
-import { Db, ObjectId, Filter, InsertOneResult, Document, Collection, InsertManyResult, MongoBulkWriteError } from 'mongodb';
-import { BaseRepository, ModelWithId, InsertedMany, PaginationResult } from '../interfaces/BaseRepository.js';
+import { Db, ObjectId, Filter, InsertOneResult, Document, Collection, InsertManyResult, MongoBulkWriteError, FindOptions } from 'mongodb';
+import { BaseRepository, ModelWithId as Model, InsertedMany, PaginationResult } from '../interfaces/BaseRepository.js';
 import Mongo from '../../db/drivers/Mongo.js';
 import { IPaginator } from '../interfaces/Paginator.js';
 import Paginator from './Paginator.js';
 
+type ModelWithId<T> = Model<T, '_id', ObjectId>;
+
 export default class MongoRepository<T extends Document> implements BaseRepository<T, '_id', ObjectId> {
-    private db: Db;
+    protected db: Db;
     protected paginator: IPaginator;
     protected collection: Collection<T>;
     protected collectionName: string;
@@ -27,7 +29,7 @@ export default class MongoRepository<T extends Document> implements BaseReposito
         return this.paginator;
     }
 
-    async findAllPaginated (item: Filter<T>, page: number): Promise<PaginationResult<T, '_id', ObjectId>> {
+    cursorPagination (item: Filter<T>, page: number) {
         const pageSize = this.paginator.getPageSize();
         const filter = {
             $facet: {
@@ -35,27 +37,32 @@ export default class MongoRepository<T extends Document> implements BaseReposito
                 data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }]
             }
         };
-        const [values] = await this.collection
-            .aggregate([{ $match: item }, filter]).toArray() as Document[1];
+
+        return this.collection
+            .aggregate<{metadata: [{totalCount: number}], data: ModelWithId<T>[]}>([{ $match: item }, filter]);
+    }
+
+    async findAllPaginated (item: Filter<T>, page: number): Promise<PaginationResult<T, '_id', ObjectId>> {
+        const [values] = await this.cursorPagination(item, page).toArray();
 
         return {
-            data: values.data,
-            pagination: this.paginator.buildMetaData(page, values.metadata[0].totalCount)
+            data: values?.data || [],
+            pagination: this.paginator.buildMetaData(page, values?.metadata[0]?.totalCount || 0)
         };
     }
 
-    insert (item: T): Promise<ModelWithId<T, '_id', ObjectId>> {
+    insert (item: T): Promise<ModelWithId<T>> {
         const fields: T = { created_at: new Date(), updated_at: null, ...item };
         return this.collection.insertOne(fields as any)
             .then((result: InsertOneResult<T>) => (
-                    { ...{ _id: result.insertedId }, ...fields } as ModelWithId<T, '_id', ObjectId>
+                    { ...{ _id: result.insertedId }, ...fields } as ModelWithId<T>
             ));
     }
 
     private mapData (insertIds: {[key: number]: ObjectId}, items: T[]) {
         return Object.values(insertIds)
             .map(
-                (id, index) => ({ ...{ _id: id }, ...items[index] }) as ModelWithId<T, '_id', ObjectId>
+                (id, index) => ({ ...{ _id: id }, ...items[index] }) as ModelWithId<T>
             );
     }
 
@@ -85,17 +92,17 @@ export default class MongoRepository<T extends Document> implements BaseReposito
             });
     }
 
-    updateById (id: string, item: Partial<T>): Promise<boolean | Error> {
+    updateById (id: string, item: Partial<T>): Promise<boolean> {
         const filter = { _id: new ObjectId(id) } as Filter<T>;
 
         return this.collection.updateOne(filter, { $set: { updated_at: new Date(), ...item } })
             .then(result => {
                 if (result.matchedCount === 0) {
-                    return Promise.reject(new Error('Document not found'));
+                    throw new Error('Document not found');
                 }
                 return true;
             })
-            .catch(error => (new Error(error.message || 'Update failed')));
+            .catch(error => { throw (new Error(error.message || 'Update failed')); });
     }
 
     deleteById (id: string): Promise<boolean | Error> {
@@ -121,15 +128,28 @@ export default class MongoRepository<T extends Document> implements BaseReposito
         });
     }
 
-    findById (id: string): Promise<ModelWithId<T, '_id', ObjectId> | null> {
-        return this.collection.findOne({ _id: new ObjectId(id) } as Filter<T>);
+    /**
+     *
+     * @param fields
+     * @returns FindOptions<t> projection
+     */
+    private parseFields (fields: (keyof T)[]): FindOptions<T>['projection'] {
+        return Object.fromEntries(Object.entries(fields).map(([, key]) => [key, 1]));
     }
 
-    find (item: Filter<T>): Promise<ModelWithId<T, '_id', ObjectId> | null> {
-        return this.collection.findOne(item);
+    findById<Fields extends keyof T> (id: string, fields: Fields[] = []): Promise<ModelWithId<Pick<T, Fields>> | null> {
+        const projection = this.parseFields(fields);
+        return this.collection
+            .findOne({ _id: new ObjectId(id) } as Filter<T>, { projection });
     }
 
-    findAll (item: Filter<T>): Promise<ModelWithId<T, '_id', ObjectId>[]> {
-        return this.collection.find(item).toArray();
+    find<Fields extends keyof T> (item: Filter<T>, fields: Fields[] = []): Promise<ModelWithId<Pick<T, Fields>> | null> {
+        const projection = this.parseFields(fields);
+        return this.collection.findOne(item, { projection });
+    }
+
+    findAll<Fields extends keyof T> (item: Filter<T>, fields: Fields[] = []): Promise<ModelWithId<T>[]> {
+        const projection = this.parseFields(fields);
+        return this.collection.find(item, { projection }).toArray();
     }
 }
